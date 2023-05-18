1e344ba9cdb2190444c8614cac998e8d0d1c0459
mya <gucciass68@gmail.com>
2023-05-18 08:08:05 +0000

    first commit

---
+++ vues/main.js
@@ -0,0 +1,8 @@
+loginBtn = document.querySelector('.btn-lg')
+dropdown = document.querySelector('.dashboard-nav-dropdown')
+
+loginBtn.addEventListener('')
+
+dropdown.addEventListener('onclick',()=>{
+    dropdown.classList.add('toggle')
+})
